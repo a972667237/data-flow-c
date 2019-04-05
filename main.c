a10240652758e4f4074f8a63192c7f7d377ff288
a972667237 <wuezmonster@gmail.com>
2019-04-05 21:49:03 +0800

    chore: update some var name

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 DF_TFL DF_TFL_TABLE;
 
-DF_AD DF_source_ad, DF_source_ad_b, DF_output_ad;
+DF_AD DF_source_ad_a, DF_source_ad_b, DF_output_ad;
 
 DF_FN SOURCE_A_FN, SOURCE_B_FN, S_FN;
 
@@ -46,32 +46,32 @@ void SOURCEB() {
 }
 
 void FUNS() {
-    int a, b;
-    int e;
+    int DF_source_A;
+    int DF_source_B;
+    int DF_output_A;
 
-    DF_AD_GetData(&S_FN, &a, sizeof(a), &b, sizeof(b));
+    DF_AD_GetData(&S_FN, &DF_source_A, sizeof(DF_source_A), &DF_source_B, sizeof(DF_source_B));
 
     {
         // fun S
-        e = a + b;
-        printf("total: -- %d\n", e);
+        DF_output_A = DF_source_A + DF_source_B;
+        printf("total: -- %d\n", DF_output_A);
     }
-
-    DF_AD_UpData(&DF_TFL_TABLE, &S_FN, &e, sizeof(e));
+    DF_AD_UpData(&DF_TFL_TABLE, &S_FN, &DF_output_A, sizeof(DF_output_A));
 }
 
 int main() {
-    DF_ADInit(&DF_source_ad, 4, 1);
+    DF_ADInit(&DF_source_ad_a, 4, 1);
     DF_ADInit(&DF_source_ad_b, 4, 1);
     DF_ADInit(&DF_output_ad, 4, 1);
 
     DF_FNInit1(&SOURCE_A_FN, &SOURCEA, "SA", 0);
-    DF_FNInit2(&SOURCE_A_FN, 1, &DF_source_ad);
+    DF_FNInit2(&SOURCE_A_FN, 1, &DF_source_ad_a);
 
     DF_FNInit1(&SOURCE_B_FN, &SOURCEB, "SB", 0);
     DF_FNInit2(&SOURCE_B_FN, 1, &DF_source_ad_b);
 
-    DF_FNInit1(&S_FN, &FUNS, "FS", 2, &DF_source_ad, &DF_source_ad_b);
+    DF_FNInit1(&S_FN, &FUNS, "FS", 2, &DF_source_ad_a, &DF_source_ad_b);
     DF_FNInit2(&S_FN, 1, &DF_output_ad);
 
     DF_SourceInit(&DF_TFL_TABLE, 2, &SOURCE_A_FN, &SOURCE_B_FN);
